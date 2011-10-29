7e2f4ee596acaf26e22c6e081131943e1deded99
Pablo Klijnjan <pabloklijnjan@gmail.com>
2011-10-30 00:55:22 +0200

    Trivial: testing with socialintegration

--- src/carlog/settings.py
+++ src/carlog/settings.py
@@ -96,47 +96,6 @@ TEMPLATE_LOADERS = (
 #     'django.template.loaders.eggs.Loader',
 )
 
-AUTHENTICATION_BACKENDS = (
-    'social_auth.backends.twitter.TwitterBackend',
-    'social_auth.backends.facebook.FacebookBackend',
-    'social_auth.backends.google.GoogleOAuthBackend',
-    'social_auth.backends.google.GoogleOAuth2Backend',
-    'social_auth.backends.google.GoogleBackend',
-    'social_auth.backends.yahoo.YahooBackend',
-    'social_auth.backends.contrib.linkedin.LinkedinBackend',
-    'social_auth.backends.contrib.livejournal.LiveJournalBackend',
-    'social_auth.backends.contrib.orkut.OrkutBackend',
-    'social_auth.backends.contrib.foursquare.FoursquareBackend',
-    'social_auth.backends.contrib.github.GithubBackend',
-    'social_auth.backends.OpenIDBackend',
-    'django.contrib.auth.backends.ModelBackend',
-)
-
-SOCIAL_AUTH_IMPORT_BACKENDS = (
-    'myproy.social_auth_extra_services',
-)
-
-SOCIAL_AUTH_ENABLED_BACKENDS = ('google', 'google-oauth', 'facebook', 'twitter', 'linkedin')
-
-TWITTER_CONSUMER_KEY         = ''
-TWITTER_CONSUMER_SECRET      = ''
-FACEBOOK_APP_ID              = ''
-FACEBOOK_API_SECRET          = ''
-LINKEDIN_CONSUMER_KEY        = ''
-LINKEDIN_CONSUMER_SECRET     = ''
-ORKUT_CONSUMER_KEY           = ''
-ORKUT_CONSUMER_SECRET        = ''
-GOOGLE_CONSUMER_KEY          = ''
-GOOGLE_CONSUMER_SECRET       = ''
-GOOGLE_OAUTH2_CLIENT_ID      = ''
-GOOGLE_OAUTH2_CLIENT_SECRET  = ''
-FOURSQUARE_CONSUMER_KEY      = ''
-FOURSQUARE_CONSUMER_SECRET   = ''
-
-LOGIN_URL          = '/accounts/login/'
-LOGIN_REDIRECT_URL = '/'
-LOGIN_ERROR_URL    = '/accounts/login/'
-
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -162,10 +121,33 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.admin',
     'django.contrib.admindocs',
-    'social_auth',
+    'socialregistration',
+    'socialregistration.contrib.facebook',
     'carlog.fangorn',
     'carlog.entries',
 )
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.contrib.auth.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.static",
+    "django.contrib.messages.context_processors.messages",
+    "django.core.context_processors.request",
+)
+
+AUTHENTICATION_BACKENDS = (
+        'django.contrib.auth.backends.ModelBackend',
+        'socialregistration.contrib.facebook.auth.FacebookAuth',
+)
+
+#LOGIN_URL          = '/accounts/login/'
+LOGIN_REDIRECT_URL = '/'
+#LOGIN_ERROR_URL    = '/accounts/login/'
+
+FACEBOOK_APP_ID = '298823853479572'
+FACEBOOK_SECRET_KEY = 'ae44fc452df4c392d224f7f6ad4ed5f7'
+FOURSQUARE_REQUEST_PERMISSIONS = ''
 
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
