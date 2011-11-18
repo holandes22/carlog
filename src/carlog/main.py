a23334b56512dc90782c128b5628c9a7551a0ab5
Pablo Klijnjan <pabloklijnjan@gmail.com>
2011-11-18 11:53:54 +0200

    Trivial: Cleaning up login/auth a little

--- src/carlog/main.py
+++ src/carlog/main.py
@@ -1,19 +1,18 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext as RequestContextTemplate
 from django.template.context import RequestContext
+
 from socialregistration.contrib.facebook.models import FacebookProfile
 
 def carlog_main(request):
     return render_to_response('base.html', context_instance = RequestContext(request))
 
-def auth_page(request):
-    return render_to_response('registration/auth.html', context_instance = RequestContext(request))
-
 def search_page(request):
     return render_to_response('search/search.html', context_instance = RequestContext(request))
 
-def test_auth_page(request):
-    return render_to_response('registration/logintest.html', {'facebook' : FacebookProfile.objects.all()},
+def auth_page(request):
+    return render_to_response('registration/auth.html', {'facebook' : FacebookProfile.objects.all()},
                                context_instance = RequestContextTemplate(request))
+ 
     
     
--- src/carlog/urls.py
+++ src/carlog/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib.auth.views import login, logout
-from django.views.generic.simple import redirect_to
-from carlog.main import carlog_main, auth_page, search_page,test_auth_page
+from carlog.main import carlog_main, search_page, auth_page
 
 
 from django.contrib import admin
@@ -10,7 +9,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^search/$', search_page),
     url(r'^social/', include('socialregistration.urls', namespace = 'socialregistration')),
-    url(r'^accounts/auth/$', test_auth_page),                       
+    url(r'^accounts/auth/$', auth_page),                       
     url(r'^accounts/login/$', login),
     url(r'^accounts/logout/$', logout),
     url(r'^tree/', include('carlog.fangorn.urls')),
