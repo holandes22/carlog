06b97001e85fc7f9003acffeaab188b0da6ae663
Pablo Klijnjan <pabloklijnjan@gmail.com>
2011-11-18 12:44:53 +0200

    Trivial: Added generic dialog reload function and removed unnecessary js files

--- src/carlog/static/lib/jscripts/main.js
+++ src/carlog/static/lib/jscripts/main.js
@@ -11,10 +11,28 @@ function resizeTreatmentDetailsGridSize(){
   	var wh = $(window).height();
 	$("#treatment_details_grid").setGridWidth($(window).width() - 450, true);
 	$("#treatment_details_grid").setGridHeight($(window).height() - 200, true);  	  	
-	
-	
 }
 
+function genericLoadDialog(form_selector, dialog_selector, onSuccessHandler){
+	$.ajax({
+		url: $(form_selector).attr('action'),
+		type: 'POST',
+		data:  $(form_selector).serialize(),
+		success: function(data, textStatus, jqXHR){
+			if(data == 'saved'){
+				$(dialog_selector).dialog('close');
+				if(onSuccessHandler && typeof onSuccessHandler == 'function'){
+					onSuccessHandler();
+				}
+				return;
+			}
+			//We got errors
+			$(dialog_selector).html(data);
+			$('.field_error').effect("highlight", { times: 3 }, 1200);
+		},
+	})
+	
+}
 
 $(document).ready(function() {
 	$(window).bind('resize', function() {
@@ -22,5 +40,27 @@ $(document).ready(function() {
 	});
 	resizeMainWindow();
 	resizeTreatmentDetailsGridSize();
+	
+	$( "#login_dialog" ).dialog(
+		{
+			autoOpen: false,
+		}
+	);
+	
+	$( "#editor_dialog" ).dialog({
+		autoOpen: false,
+		width: 'auto',
+		buttons: {
+			Save: function() {
+				genericLoadDialog("#editor_form", this, reactivateCurrentTreeNode);	
+			},
+			Cancel: function() {
+				$( this ).dialog( "close" );
+			},
+			
+		}
+	});
+	
+	
 });
 
