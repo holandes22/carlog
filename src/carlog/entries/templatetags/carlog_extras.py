d388f9464ee25fac1fbfd0c358f8c655993841c7
Pablo Klijnjan <pabloklijnjan@gmail.com>
2011-11-25 13:57:15 +0200

    Trivial: Added template filter to return field type

--- src/carlog/entries/templatetags/carlog_extras.py
+++ src/carlog/entries/templatetags/carlog_extras.py
@@ -5,3 +5,7 @@ register = template.Library()
 @register.filter()
 def replace_under_with(value, arg):
     return value.replace("_", arg)
+
+@register.filter()
+def field_type(field):
+    return field.field.type.__class__.name 
