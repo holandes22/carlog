ba887a88a6285081b2e27c05e4428e3268503950
Pablo Klijnjan <pabloklijnjan@gmail.com>
2012-02-10 03:58:43 +0200

    Trivial: Switched db to sqlite

--- carlog/settings.py
+++ carlog/settings.py
@@ -16,7 +16,7 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'carlog_db',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
